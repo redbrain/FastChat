090d7c7dbff6d3dc9881e3d6ea237370555372e3
redbrain <42494361+redbrain@users.noreply.github.com>
2024-02-28 15:38:26 -0500

    Create model_mamba.py

---
+++ fastchat/model/model_mamba.py
@@ -0,0 +1,20 @@
+from types import SimpleNamespace
+import warnings
+
+from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+
+
+class MambaModel:
+    def __init__(self, model_path):
+        warnings.warn("Experimental support for Mamba.")
+        
+        self.config = SimpleNamespace(is_encoder_decoder=False)
+        self.model = MambaLMHeadModel.from_pretrained(model_path)
+        self.tokenizer = None
+        self.model_path = model_path
+
+    def to(self, target):
+        assert target == "cuda"
+
+    def generate(self, input_ids, do_sample, temperature, max_new_tokens, top_k=1, top_p=0.0, min_p=0.0, **kwargs):
+        return self.model.generate(input_ids, max_new_tokens, top_k, top_p, min_p=, temperature, **kwargs)
